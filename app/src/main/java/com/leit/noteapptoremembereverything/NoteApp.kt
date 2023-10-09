5270a65ad64dcbc3e71d5540892b0df6c0aa1f24
Vladyslav Kyrychok <vladyslav.kyrychokv@gmail.com>
2023-10-09 17:21:34 +0300

    Dependency injection

---
+++ app/src/main/java/com/leit/noteapptoremembereverything/NoteApp.kt
@@ -0,0 +1,7 @@
+package com.leit.noteapptoremembereverything
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApp:Application()
---
+++ app/src/main/java/com/leit/noteapptoremembereverything/di/AppModule.kt
@@ -0,0 +1,44 @@
+package com.leit.noteapptoremembereverything.di
+
+import android.app.Application
+import androidx.room.Room
+import com.leit.noteapptoremembereverything.feature_note.data.data_source.NoteDatabase
+import com.leit.noteapptoremembereverything.feature_note.data.repository.NoteRepositoryImp
+import com.leit.noteapptoremembereverything.feature_note.domain.repository.NoteRepository
+import com.leit.noteapptoremembereverything.feature_note.domain.use_case.DeleteNote
+import com.leit.noteapptoremembereverything.feature_note.domain.use_case.GetNotes
+import com.leit.noteapptoremembereverything.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app:Application):NoteDatabase{
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNotesRepository(db:NoteDatabase):NoteRepository{
+        return NoteRepositoryImp(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository):NoteUseCases{
+        return NoteUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
+    }
+}
